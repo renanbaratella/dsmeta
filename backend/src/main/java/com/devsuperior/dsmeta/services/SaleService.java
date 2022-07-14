c1ee08817e150548417335b2c924bdea47bec3dc
Renan <baratellarenan@gmail.com>
2022-07-13 23:09:46 -0300

    API teste concluido

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,12 +1,8 @@
 package com.devsuperior.dsmeta.services;
 
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.ZoneId;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -17,14 +13,8 @@ public class SaleService {
 
 	@Autowired
 	private SaleRepository repository;
-	
-	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
-		
-		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
-		
-		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
-		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
-		
-		return repository.findSales(min, max, pageable); 
+
+	public List<Sale> findSales() {
+		return repository.findAll();
 	}
 }
